c2c552aaa660192cc7066b3208688a674c8f2bf0
chae-munho <ansgh4300@naver.com>
2025-08-13 23:18:12 +0900

    second commit

--- components/main-header.js
+++ components/main-header.js
@@ -1,13 +1,15 @@
 import Link from "next/link"
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import Image from "next/image";
 export default function MainHeader() {
     return (
-        <header>
-            <Link href="/">
-                <img src={logoImg.src} alt="A plate with food on it"></img>
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <Image src={logoImg} alt="A plate with food on it" priority></Image>
                 NextLevel Food
             </Link>
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Browse Meals</Link>
