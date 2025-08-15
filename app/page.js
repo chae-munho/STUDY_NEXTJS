d2310a5fd3076e14809c141c797178b9e0c815a6
chae-munho <ansgh4300@naver.com>
2025-08-15 21:47:34 +0900

    108강 진행할 차례

--- app/page.js
+++ app/page.js
@@ -1,25 +1,26 @@
-import Link from "next/link";
+import Link from 'next/link';
+//컴포넌트 파일 자체가 useState를 쓰는 경우 : 그 파일 맨 위에 'use Client' 추가
+//자식 컴포넌트에서만 useState를 쓰는 경우 : 자식 컴포넌트 파일에만 'use Client' 추가하고 부모는 서버 컴포넌트로 둬도 됨
 import classes from './page.module.css';
-// localhost:3000/
+import ImageSlideshow from '@/components/images/image-slideshow';
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
         <div className={classes.slideshow}>
-
+          <ImageSlideshow></ImageSlideshow>
         </div>
-
-        <div className={classes.hero}>
-          <h1>NextLevel Food for NextLevel Foodies</h1>
-          <p>Taste & share food from all over the world.</p>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
         </div>
-
-        <div className={classes.cta}>
-          <Link href="/community">Join the Community</Link>
-          <Link href="/meals">Explore Meals</Link>
-        </div>
-
       </header>
       <main>
         <section className={classes.section}>
